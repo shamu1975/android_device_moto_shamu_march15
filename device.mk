02672e858efef87ead990a7258735a5a99b85e0c
Mikhail Naganov <mnaganov@google.com>
2017-03-27 09:47:09 -0700

    Bring up binderized Soundtrigger services
    Verified: audio in Hangouts, Play Music + EQ, YouTube,
        audio over A2DP and USB.
    
    Soundtrigger seems to be working--but only with screen on.
    Looks to me more like an issue with Assistant rather than with s/t.
    
    -shamu edit by moepda
    -edit manifest entry and device to add extra impl & services
    -no need to remove files to have ok google (screen on)
    
    Bug: 35808308
    Bug: 36098105
    Test: manual
    Change-Id: I03c0e04a8f35b7da0bbef9dd32e606a07067ba36
    Signed-off-by: moepda <moepda@gmail.com>

--- device.mk
+++ device.mk
@@ -205,7 +205,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
 
 # DRM HAL
 PRODUCT_PACKAGES += \
